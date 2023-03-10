317a1dd7a00c7c743db8ecbafbe0255a0665a610
leandroJMartin <leandro.dev@outlook.com.br>
2023-03-10 12:34:17 -0300

    Insert vericifation resize and size devices

--- src/components/layout/WhatsApp.tsx
+++ src/components/layout/WhatsApp.tsx
@@ -10,14 +10,31 @@ interface Props {
 const WhatsApp = ({ data }: Props) => {
   const number = data.coWhatsapp?.replace(/\s/g, '');
 
+  const [device, setDevice] = useState(false);
+  function CheckResize() {
+    if (window.innerWidth < 1070) {
+      setDevice(true);
+    } else {
+      setDevice(false);
+    }
+  }
+
+  useEffect(() => {
+    CheckResize();
+    window.addEventListener('resize', CheckResize);
+    return () => window.removeEventListener('resize', CheckResize);
+  }, []);
+
   return (
     <div className="fixed bottom-6 right-6 z-50">
-      <a href={`tel:${data.coTelefone}`}>
-        <MdWifiCalling3
-          size={35}
-          className="text-black bg-green rounded-full py-1 px-1 block mb-3"
-        />
-      </a>
+      {device && (
+        <a href={`tel:${data.coTelefone}`}>
+          <MdWifiCalling3
+            size={35}
+            className="text-black bg-green rounded-full py-1 px-1 block mb-3"
+          />
+        </a>
+      )}
 
       <a href={`https://api.whatsapp.com/send?phone=55${number}`}>
         <RiWhatsappFill size={40} className="text-green" />
